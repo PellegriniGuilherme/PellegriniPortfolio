40a575d84ec4b50b5d457040c59363f8a25102f4
GuilhermePellegrini <pellegrini458@gmail.com>
2022-11-13 23:33:24 -0300

    stacks add re-order

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -4,7 +4,7 @@ import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import { MdOutgoingMail } from "react-icons/md";
 import { FaLaravel, FaPhp, FaReact } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
-import { SiExpo, SiTailwindcss, SiTypescript } from "react-icons/si";
+import { SiCss3, SiExpo, SiHtml5, SiTailwindcss, SiTypescript } from "react-icons/si";
 import Popper from '@/Components/Popper';
 
 function Home() {
@@ -18,48 +18,54 @@ function Home() {
             </p>
             <div className="flex flex-row gap-x-5 justify-center items-center">
                 <a href="https://www.linkedin.com/in/pellegrini-coelho/" target="_blank">
-                <AiFillLinkedin
-                    className="text-cinza-200 cursor-pointer hover:text-cinza-100 transition-all"
-                    size={30}
-                />
+                    <AiFillLinkedin
+                        className="text-cinza-200 cursor-pointer hover:text-cinza-100 transition-all"
+                        size={30}
+                    />
                 </a>
                 <a href="https://github.com/PellegriniGuilherme" target="_blank">
-                <AiFillGithub
-                    className="text-cinza-200 cursor-pointer hover:text-cinza-100 transition-all"
-                    size={30}
-                />
+                    <AiFillGithub
+                        className="text-cinza-200 cursor-pointer hover:text-cinza-100 transition-all"
+                        size={30}
+                    />
                 </a>
                 <a href="mailto:contato@guilhermepellegrini.com.br" target="_blank">
-                <MdOutgoingMail
-                    className="text-cinza-200 cursor-pointer hover:text-cinza-100 transition-all"
-                    size={34}
-                />
+                    <MdOutgoingMail
+                        className="text-cinza-200 cursor-pointer hover:text-cinza-100 transition-all"
+                        size={34}
+                    />
                 </a>
             </div>
             <p className="uppercase text-cinza-100 text-lg font-bold">
                 Stacks
             </p>
             <div className="flex flex-row gap-x-6 md:gap-x-16 justify-center items-center">
-                <Popper text="Laravel">
-                    <FaLaravel className="text-[#ff2d20] transition-all" size={26} />
+                <Popper text="HTML 5">
+                    <SiHtml5 className="text-[#DD4B25]" size={32}/>
+                </Popper>
+                <Popper text="CSS 3">
+                    <SiCss3 className="text-[#016AB1]" size={32}/>
                 </Popper>
                 <Popper text="PHP">
-                    <FaPhp className="text-[#7a86b8] transition-all" size={32}/>
+                    <FaPhp className="text-[#7A86B8]" size={32}/>
                 </Popper>
                 <Popper text="JavaScript">
-                    <IoLogoJavascript className="text-[#efd81d] transition-all" size={26}/>
+                    <IoLogoJavascript className="text-[#EFD81F]" size={26}/>
                 </Popper>
-                <Popper text="React">
-                    <FaReact className="text-[#61dafb] transition-all" size={26}/>
-                </Popper>
-                <Popper text="Expo">
-                    <SiExpo className="text-[#bcc3cd] transition-all" size={26}/>
+                <Popper text="Laravel">
+                    <FaLaravel className="text-[#FF2D20]" size={26} />
                 </Popper>
                 <Popper text="TypeScript">
-                    <SiTypescript className="text-[#2f74c0] transition-all" size={26}/>
+                    <SiTypescript className="text-[#2F74C0]" size={26}/>
+                </Popper>
+                <Popper text="React">
+                    <FaReact className="text-[#61DAFB]" size={26}/>
+                </Popper>
+                <Popper text="Expo">
+                    <SiExpo className="text-[#BCC3CD]" size={26}/>
                 </Popper>
                 <Popper text="TailwindCss">
-                    <SiTailwindcss className="text-[#38bdf8] transition-all" size={26}/>
+                    <SiTailwindcss className="text-[#38BDF8]" size={26}/>
                 </Popper>
             </div>
         </div>
